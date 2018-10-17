a9545230353f22abbdeeffdd173eeaaee1a5f1c7
LAPTOP-FBE085SS\jaker <jakerelmi@hotmail.com>
2018-10-17 15:50:35 -0500

    Updated markdown values Updated displaying markdown values if there are any

--- StoreKata/StoreKata/ItemManager.cs
+++ StoreKata/StoreKata/ItemManager.cs
@@ -8,6 +8,7 @@ namespace StoreKata
 
         private List<Item> items = new List<Item>();
         private float totalPrice = 0.0f;
+        private string markedDownText = "";
 
         public struct Item
         {
@@ -19,6 +20,8 @@ namespace StoreKata
 
             // Updates after scan
             public float price;
+
+            public float markDownAmount;
 
             public Type type;
             public enum Type { Weighed, Each };
@@ -34,13 +37,15 @@ namespace StoreKata
             testItem1.value = 2.50f;
             testItem1.price = 0.0f;
             testItem1.type = Item.Type.Each;
+            testItem1.markDownAmount = 0.2f;
 
             Item testItem2;
             testItem2.name = "Soup";
             testItem2.quanity = 6;
-            testItem2.value = 2.50f;
+            testItem2.value = 2.70f;
             testItem2.price = 0.0f;
             testItem2.type = Item.Type.Each;
+            testItem2.markDownAmount = 1.0f;
 
             Item testItem3;
             testItem3.name = "Bananas";
@@ -48,13 +53,15 @@ namespace StoreKata
             testItem3.value = 1.89f;
             testItem3.price = 0.0f;
             testItem3.type = Item.Type.Weighed;
-
-            // Markdown Item Test
-            testItem1 = Markdown(testItem1, 0.2f);
-
-            //Add Item Test
+            testItem3.markDownAmount = 0.0f;
+            
+            testItem1 = Markdown(testItem1, testItem1.markDownAmount);
             ScanItem(testItem1);
+
+            testItem2 = Markdown(testItem2, testItem2.markDownAmount);
             ScanItem(testItem2);
+
+            testItem3 = Markdown(testItem3, testItem3.markDownAmount);
             ScanItem(testItem3);
 
             DisplayCheckout();
@@ -77,8 +84,14 @@ namespace StoreKata
         // Markdown Item Test
         public Item Markdown(Item storeItem, float amount)
         {
+            if (amount <= 0.0f)
+                return storeItem;
+
             Item item = storeItem;
             item.value -= amount;
+
+            markedDownText = string.Format("\tMarked-Down from $ {0 :}!", storeItem.value);
+
             return item;
            
         }
@@ -97,12 +110,17 @@ namespace StoreKata
             {
                 Console.WriteLine("- " + item.name + "\t" + item.quanity + "lbs." + " ($" + item.value + " / lb.) " + "\n\t\t\t\t\t$" + item.price);
             }
-            
+
+            if(item.markDownAmount > 0.0f)
+                Console.WriteLine(markedDownText);
+
+
+            Console.WriteLine("------------------------------------------------------");
+
         }
 
         private void DisplayCheckout()
         {
-            Console.WriteLine("------------------------------------------------------");
             Console.WriteLine("\n\t\t\t\t Total: $" + totalPrice);
         }
 
