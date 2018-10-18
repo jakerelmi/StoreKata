11363b283266148b0ff089b50b123b6ab7030938
LAPTOP-FBE085SS\jaker <jakerelmi@hotmail.com>
2018-10-17 20:51:08 -0500

    Adjusted display formatting Supported applying specials or coupons per non weighed items

--- StoreKata/StoreKata/ItemManager.cs
+++ StoreKata/StoreKata/ItemManager.cs
@@ -9,6 +9,7 @@ namespace StoreKata
         private List<Item> items = new List<Item>();
         private float totalPrice = 0.0f;
         private string markedDownText = "";
+        private string specialOfferText = "";
 
         public struct Item
         {
@@ -33,11 +34,11 @@ namespace StoreKata
             // Test Items
             Item testItem1;
             testItem1.name = "Milk";
-            testItem1.quanity = 1;
+            testItem1.quanity = 3;
             testItem1.value = 2.50f;
             testItem1.price = 0.0f;
             testItem1.type = Item.Type.Each;
-            testItem1.markDownAmount = 0.2f;
+            testItem1.markDownAmount = 0.0f;
 
             Item testItem2;
             testItem2.name = "Soup";
@@ -55,20 +56,27 @@ namespace StoreKata
             testItem3.type = Item.Type.Weighed;
             testItem3.markDownAmount = 0.0f;
             
+            // Milk
             testItem1 = Markdown(testItem1, testItem1.markDownAmount);
             ScanItem(testItem1);
 
+            // Soup
             testItem2 = Markdown(testItem2, testItem2.markDownAmount);
             ScanItem(testItem2);
 
+            // Bananas
             testItem3 = Markdown(testItem3, testItem3.markDownAmount);
             ScanItem(testItem3);
 
-            DisplayCheckout();
+            // Apply specials
+            BuyNItemsGetMOffSpecial(testItem1, 2, 0.5f); // Buy 2 get next 50% off on Milk
+            BuyNItemsGetMOffSpecial(testItem2, 1, 1.0f); // Buy 1 get 1 free soup
+
+            Console.WriteLine("\n\t\t\t\t\t Total: $" + totalPrice);
         }
 
         // Add Item Test
-        public void ScanItem(Item storeItem)
+        private void ScanItem(Item storeItem)
         {
             storeItem.price = storeItem.value * storeItem.quanity;
 
@@ -78,11 +86,11 @@ namespace StoreKata
             // Add item to list
             items.Add(storeItem);
 
-            UpdateCheckout(storeItem);
+            DisplayCheckout(storeItem);
         }
 
         // Markdown Item Test
-        public Item Markdown(Item storeItem, float amount)
+        private Item Markdown(Item storeItem, float amount)
         {
             if (amount <= 0.0f)
                 return storeItem;
@@ -90,39 +98,57 @@ namespace StoreKata
             Item item = storeItem;
             item.value -= amount;
 
-            markedDownText = string.Format("\tMarked-Down from $ {0 :}!", storeItem.value);
+            markedDownText = string.Format("\t-Marked-Down from ${0 :}!", storeItem.value);
 
             return item;
            
         }
 
-        // Display Methods
-        private void UpdateCheckout(Item item)
+        // Special Item Test
+        private void BuyNItemsGetMOffSpecial(Item storeItem, int quanitityQualification, float amount)
+        {
+            // Only apply to non-weighed items
+            if (storeItem.type != Item.Type.Each)
+                return;
+
+            if (storeItem.quanity > quanitityQualification)
+            {
+                // Apply discount after scan 
+                float discountAmount = amount * storeItem.value;
+                totalPrice -= discountAmount;
+
+                // Display to customer
+                specialOfferText = string.Format("-Applied Buy {0} get 1 {1}% off special for {2}!\n\t\t\t\t\t\t- ${3}", quanitityQualification, amount * 100, storeItem.name, discountAmount);
+            }
+            
+            if (specialOfferText != "")
+                Console.WriteLine(specialOfferText);
+        }
+
+        // Display 
+        private void DisplayCheckout(Item item)
         {
             if (item.type == Item.Type.Each)
             {
                 if(item.quanity > 1)
-                    Console.WriteLine("- " + item.name + "\t\t" + item.quanity + "X" + " ($" + item.value + " each) " + "\n\t\t\t\t\t$" + item.price);
+                    Console.WriteLine("- " + item.name + "\t\t" + item.quanity + "X" + " ($" + item.value + " each) " + "\n\t\t\t\t\t\t$" + item.price);
                 else
-                    Console.WriteLine("- " + item.name + "\t\t\t\t\t$" + item.price);
+                    Console.WriteLine("- " + item.name + "\t\t\t\t\t\t$" + item.price);
             }
             else
             {
-                Console.WriteLine("- " + item.name + "\t" + item.quanity + "lbs." + " ($" + item.value + " / lb.) " + "\n\t\t\t\t\t$" + item.price);
+                Console.WriteLine("- " + item.name + "\t" + item.quanity + " lbs." + " ($" + item.value + " / lb.) " + "\n\t\t\t\t\t\t$" + item.price);
             }
 
             if(item.markDownAmount > 0.0f)
                 Console.WriteLine(markedDownText);
 
 
+            specialOfferText = "";
+
+
             Console.WriteLine("------------------------------------------------------");
 
         }
-
-        private void DisplayCheckout()
-        {
-            Console.WriteLine("\n\t\t\t\t Total: $" + totalPrice);
-        }
-
     }
 }
